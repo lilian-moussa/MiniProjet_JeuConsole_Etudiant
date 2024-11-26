8894f13bbcc5b5d6d0fc637ba33db5df7b4f8342
lilian.m <lilian.m@lycee-de-beauvoir.education>
2024-11-26 17:48:41 +0100

    Commit  (a modifier)

--- main.cpp
+++ main.cpp
@@ -2,44 +2,62 @@
 using namespace std;
 
 void zanzibarV1();
-void zanzibarV2();
+
 
 int main() {
 
     zanzibarV1();
-    zanzibarV2();
+
     return 0;
 }
 
+//Fonction de zanzibar
 void zanzibarV1(){
     //Variables des 3 lancers de des
     int de1=0;
     int de2=0;
     int de3=0;
+    int totalScore=0; //Score total du joueur
 
-    // Demander à l'utlisateur la valeur des 3 des
-    cout<<" Saisir la valeur du premier de (entre 1 et 6) ";
-    cin>>de1;
+    // Jouer pendant 3 tours
+    for (int tour = 1; tour <= 3; tour++) {
+        cout << "Tour " << tour << " : " << endl;
 
-    cout<<" Saisir la valeur du second de (entre 1 et 6) ";
-    cin>>de2;
+        // Demander à l'utlisateur la valeur des 3 des
+        cout << " Saisir la valeur du premier de (entre 1 et 6) " << endl;
+        cin >> de1;
 
-    cout<<" Saisir la valeur du troisieme de (entre 1 et 6) ";
-    cin>>de3;
-}
+        cout << " Saisir la valeur du second de (entre 1 et 6) " << endl;
+        cin >> de2;
 
-void zanzibarV2(){
-    //Tableau comprenant les valeurs d'un lancer
-    int de[3] = {0};
+        cout << " Saisir la valeur du troisieme de (entre 1 et 6) " << endl;
+        cin >> de3;
 
-    // Demander à l'utlisateur la valeur des 3 des
-    cout<<" Saisir la valeur du premier de (entre 1 et 6) ";
-    cin>>de[0];
+        // Vérifier si les valeurs sont valides (entre 1 et 6)
+        if ((de1 >= 1 && de1 <= 6) && (de2 >= 1 && de2 <= 6) && (de3 >= 1 && de3 <= 6)) {
+            cout << "Les valeurs des lancers sont valides." << endl;
 
-    cout<<" Saisir la valeur du second de (entre 1 et 6) ";
-    cin>>de[1];
+        } else {
+            cout << "Une ou plusieurs valeurs sont incorrectes. Les valeurs doivent être entre 1 et 6." << endl;
+        }
 
-    cout<<" Saisir la valeur du troisieme de (entre 1 et 6) ";
-    cin>>de[2];
+        // Affichage du score total après ce tour
+        cout << "Score total après ce tour : " << totalScore << endl << endl;
+
+        if (de1 || de2 || de3 == 1){
+            totalScore=100;
+        }
+        else
+            if(de1 || de2 || de3 == 6){
+                totalScore=60;
+            }
+            else{
+                totalScore= de1 + de2 + de3;
+            }
+
+    }
+
+    // Affichage du score final après 3 tours
+    cout << "Le score final du joueur après 3 tours est : " << totalScore << endl;
 
 }
