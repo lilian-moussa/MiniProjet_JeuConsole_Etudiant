39600842ff9251fd1f00688cbb3bf93b178f022f
lilian.m <lilian.m@lycee-de-beauvoir.education>
2024-11-26 15:36:59 +0100

    Initialisation des variables+Demande utilsateur valeur des 3 des

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//Tableau comprenant les valeurs d'un lancer
+    int valeurDes[6] = {100,2,3,4,5,60};
+//Variables des 3 lancers de des
+    int lancer1=0;
+    int lancer2=0;
+    int lancer3=0;
+// Demander à l'utlisateur la valeur des 3 des
+    cout<<" Saisir la valeur du premier de (entre 1 et 6) ";
+    cin>>lancer1;
+
+    cout<<" Saisir la valeur du second de (entre 1 et 6) ";
+    cin>>lancer2;
+
+    cout<<" Saisir la valeur du troisieme de (entre 1 et 6) ";
+    cin>>lancer3;
+
+
+
+
+
+
     return 0;
 }
