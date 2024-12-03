adaa689dbbabdd652da7ccd9989533967cdf9d95
lilian.m <lilian.m@lycee-de-beauvoir.education>
2024-12-03 15:07:26 +0100

    Zanzibar Niveau0

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ void zanzibarV1(){
     int de1=0;
     int de2=0;
     int de3=0;
+    int scoreTour=0; //Score de chaque tours
     int totalScore=0; //Score total du joueur
 
     // Jouer pendant 3 tours
@@ -41,23 +42,27 @@ void zanzibarV1(){
             cout << "Une ou plusieurs valeurs sont incorrectes. Les valeurs doivent être entre 1 et 6." << endl;
         }
 
-        // Affichage du score total après ce tour
-        cout << "Score total après ce tour : " << totalScore << endl << endl;
-
-        if (de1 || de2 || de3 == 1){
-            totalScore=100;
+        //Boucle des 3 lancés de dés
+        if (de1 == 1 || de2 == 1 || de3 == 1){ //Cas ou dés = 1
+            scoreTour=100;
         }
         else
-            if(de1 || de2 || de3 == 6){
-                totalScore=60;
+            if(de1 == 6 || de2 == 6 || de3 == 6){ // Cas ou dés = 6
+                scoreTour=60;
             }
-            else{
-                totalScore= de1 + de2 + de3;
+            else
+                if((de1 == 1 || de2 == 1 || de3 == 1) || (de1 == 6 || de2 == 6 || de3 == 6)){ //cas ou dés = 1 et 6
+                    scoreTour= 100;
             }
+                else{    //Cas ou dés = autres choses que 1 ou 6
+
+                    scoreTour= de1 + de2 + de3;
+                }
+        totalScore += scoreTour;
+        cout << "Score total apres ce tour : " << totalScore << endl << endl;//Affiche le score de chaque tours
 
     }
 
     // Affichage du score final après 3 tours
-    cout << "Le score final du joueur après 3 tours est : " << totalScore << endl;
-
+    cout << "Le score final du joueur apres 3 tours est : " << totalScore << endl;
 }
