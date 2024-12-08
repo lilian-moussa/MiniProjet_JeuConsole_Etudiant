47726738349dac365f2a8372c693b6f842d06521
lilia <lilian.m@lycee-de-beauvoir.education>
2024-12-08 17:19:01 +0100

    Rajout documentation

---
+++ .h
@@ -0,0 +1,13 @@
+/**
+ *@file MiniProjet_JeuConsole_Etudiant
+ *@brief Le projet est un programme pour joueur aux zanzibar, un jeu qui se joue en 3lancers de dés par
+ *@brief joueurs sur 3 tours. A la fin des 3 tours le joueur avec le plus de points gagne.
+ *@author Moussa Lilian
+ *@date 08/12/2024
+*/
+
+/**
+ *@fn void zanzibarV1
+ *@brief C'est la fonction principale pour faire focntionner le jeu du zanzibar
+ */
+
